347413929af62f8d56da97e0135b4f9e56c3ca03
Harish Kumar <saiharish120896@gmail.com>
2017-05-03 19:33:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.post('/login',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    if((username == "saiharish96" && password == "kavyasri") || (username == "kavyasri" && password == "saiharish96")){
+        res.status(200).send("YOU BOTH MAKE A NICE COUPLE");
+    }
+    else{
+        res.status(500).send("FAILURE INVALID CREDENTIALS");
+    }
+})
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
